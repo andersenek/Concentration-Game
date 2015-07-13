b916720acb9afe0799142def145d4f4f91250855
Erica Andersen <andersenek@gmail.com>
2015-07-13 10:22:27 -0400

    Added basic HTML structure and linked CSS and JS files.

---
+++ js/game.js
@@ -0,0 +1,15 @@
+// Concentration Game: The object of the game is to collect as many matching pairs as possible.
+
+// Start with a standard deck of playing cards
+
+// Lay the cards out face down in rows forming a large rectangle on the table or floor. Make sure the cards are not touching each other. They need to be able to be flipped over without disturbing any cards around them.
+
+// The first player chooses a card and turns it over.
+
+// The player then selects another card and turns it over.
+// If the two cards are a matching pair for example two sixes then they take the two cards and start their stack.
+// If the cards are not a match they are turned back over and resume playing.
+
+// Play continues in this fashion until all the cards are played or time runs out.
+
+// A players turn is not over until they are unsuccessfully at matching a pair.
