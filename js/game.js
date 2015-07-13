c9ecd3e23821b2567ae70c8edd3920243a6f63ee
Erica Andersen <andersenek@gmail.com>
2015-07-13 11:52:51 -0400

    Added click listeners and functionality for cards to show on click.

--- js/game.js
+++ js/game.js
@@ -1,15 +1,23 @@
 // Concentration Game: The object of the game is to collect as many matching pairs as possible.
 
-// Start with a standard deck of playing cards
+// Start with a standard deck (grid) of playing cards.
 
-// Lay the cards out face down in rows forming a large rectangle on the table or floor. Make sure the cards are not touching each other. They need to be able to be flipped over without disturbing any cards around them.
-
-// The first player chooses a card and turns it over.
-
+// The player can chooses a card and turn it over.
 // The player then selects another card and turns it over.
+
 // If the two cards are a matching pair for example two sixes then they take the two cards and start their stack.
 // If the cards are not a match they are turned back over and resume playing.
 
 // Play continues in this fashion until all the cards are played or time runs out.
 
 // A players turn is not over until they are unsuccessfully at matching a pair.
+
+var grid = $("#memory-grid"), // Create grid variable
+gridCards = []; // Create card array
+
+  grid.on("click", "li.card", function() { // Add Click Listener for list items .card in #memory-grid
+    var self = $(this);
+
+    gridCards.push(self.addClass("check")); // Set class 'check' to see if card has been clicked and push to array
+    console.log("The card has been clicked")
+});
